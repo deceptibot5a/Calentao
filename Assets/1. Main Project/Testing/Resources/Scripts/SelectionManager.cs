758cc38d4f0d6b9bff5ac5ba5c7c892fe5e1785d
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-02-13 16:03:18 -0500

    Object outline with raycast

---
+++ Assets/1. Main Project/Testing/Resources/Scripts/SelectionManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private float distance = 3f;
+    private Transform highlight;
+    private Transform selection;
+
+
+    void Update()
+    {
+        if (highlight != null)
+        {
+            highlight.gameObject.GetComponent<Outline>().enabled = false;
+            highlight = null;
+        }
+        
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.green);
+
+        RaycastHit hitinfo;
+        if (Physics.Raycast(ray, out hitinfo, distance))
+        {
+            highlight = hitinfo.transform;
+            if (highlight.CompareTag("Selectable") && highlight != selection)
+            {
+                if (highlight.gameObject.GetComponent<Outline>() != null)
+                {
+                    highlight.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    Outline outline = highlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.magenta;
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+    }
+}
+    
+
