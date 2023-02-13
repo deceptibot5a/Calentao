67dd6cd7b9e35e38059e76a109f544f61bc7c8f0
QuantumBreak3678 <68030853+juandagongoradiaz@users.noreply.github.com>
2023-02-13 10:35:07 -0500

    Character movement 1

--- Assets/1. Main Project/Testing/Resources/Scripts/InputManager.cs
+++ Assets/1. Main Project/Testing/Resources/Scripts/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,59 @@ namespace Calentao.PlayerContol
 {
     public class InputManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private PlayerInput PlayerInput; 
+        
+        public Vector2 Move {get; private set;}
+        
+        public Vector2 Look {get; private set;}
+        
+        public bool Run {get; private set;}
+
+        private InputActionMap _currentMap;
+
+        private InputAction _moveAction;
+        
+        private InputAction _lookAction;
+
+        private InputAction _runAction;
+
+        private void Awake()
         {
+            _currentMap = PlayerInput.currentActionMap;
+            _moveAction = _currentMap.FindAction("Move");
+            _lookAction = _currentMap.FindAction("Look");
+            _runAction = _currentMap.FindAction("Run");
+
+            _moveAction.performed += onMove;
+            _lookAction.performed += onLook;
+            _runAction.performed += onRun; 
             
+            _moveAction.canceled += onMove;
+            _lookAction.canceled += onLook;
+            _runAction.canceled += onRun; 
+        }
+        
+        private  void onMove(InputAction.CallbackContext context)
+        {
+            Move = context.ReadValue<Vector2>();
+        }
+        private  void onLook(InputAction.CallbackContext context)
+        {
+            Look = context.ReadValue<Vector2>();
+        }
+        private  void onRun(InputAction.CallbackContext context)
+        {
+            Run = context.ReadValueAsButton(); 
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnEnable()
         {
-            
+            _currentMap.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _currentMap.Disable();
         }
     }
 }
--- Assets/1. Main Project/Testing/Resources/Scripts/PlayerController.cs
+++ Assets/1. Main Project/Testing/Resources/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,57 @@ namespace Calentao.PlayerContol
 {
     public class PlayerController : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private float AnimBlendSpeed = 8.9f; 
+        
+        private Rigidbody _playerRigidbody;
+        private InputManager _inputManager;
+
+        private Animator _animator;
+
+        private bool _hasAnimator;
+
+        private int _xVelHash;
+        private int _yVelHash;
+
+        private const float _walkSpeed = 2f; 
+        
+        private const float _runSpeed = 6f;
+
+        private Vector2 _currentVelocity; 
+        
         void Start()
         {
-            
+            _hasAnimator = TryGetComponent<Animator>(out _animator);
+            _playerRigidbody = GetComponent<Rigidbody>();
+            _inputManager = GetComponent<InputManager>();
+
+            _xVelHash = Animator.StringToHash("X_Velocity");
+            _yVelHash = Animator.StringToHash("Y_Velocity"); 
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (!_hasAnimator) return;
+
+            float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
+            if (_inputManager.Move == Vector2.zero) targetSpeed = 0.1f;
+
+            _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * targetSpeed, AnimBlendSpeed * Time.fixedDeltaTime); 
+            _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * targetSpeed, AnimBlendSpeed * Time.fixedDeltaTime); 
+
+            var xVelDifference = _currentVelocity.x - _playerRigidbody.velocity.x; 
+            var zVelDifference = _currentVelocity.y - _playerRigidbody.velocity.z;
             
+            _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelDifference, 0 ,zVelDifference)), ForceMode.VelocityChange);
+            
+            _animator.SetFloat(_xVelHash , _currentVelocity.x);
+            _animator.SetFloat(_yVelHash, _currentVelocity.y);
+
         }
     }
 }
