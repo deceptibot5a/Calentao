b796da5dc4a137a805ba3daf2ca56d313a10fd88
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-03-24 09:17:08 -0500

    Selection player

--- Assets/1. Main Project/Scripts/Menu/MenuManager_Test.cs
+++ Assets/1. Main Project/Scripts/Menu/MenuManager_Test.cs
@@ -5,14 +5,36 @@ using Unity.Collections.LowLevel.Unsafe;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class MenuManager_Test : MonoBehaviour
 {
-    public static MenuManager_Test Instance;
+    public enum JugadoresMinimos
+    {
+        Minimo1Jugador = 1,
+        Minimo2Jugador = 2
+    }
     
+    
+    public static MenuManager_Test Instance;
     [SerializeField] private Menu_test[] menus;
     [SerializeField] private GameObject player1, player2;
-    
+    [SerializeField] private Button startButton;
+    private bool player1Selected = false;
+    private bool player2Selected = false;
+    public JugadoresMinimos jugadoresMinimos;
+
+    private void Update()
+    {
+        if (PhotonNetwork.PlayerList.Length == (int)jugadoresMinimos)
+        {
+            startButton.interactable = true;
+        }
+        else
+        {
+            startButton.interactable = false;
+        }
+    }
 
     private void Awake()
     {
@@ -55,11 +77,14 @@ public class MenuManager_Test : MonoBehaviour
     {
         player1.GetComponent<Image>().color = Color.green;
         Destroy(player2);
+        player1Selected = true;
+        
     }
     public void Player2Selected()
     {
         player2.GetComponent<Image>().color = Color.green;
         Destroy(player1);
+        player2Selected = true;
     }
     
 }
