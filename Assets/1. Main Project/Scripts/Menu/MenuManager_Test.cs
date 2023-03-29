f2e3468562991999e67f69535017ad23c0d27ac2
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-03-29 12:44:57 -0500

    Cambios alfa

--- Assets/1. Main Project/Scripts/Menu/MenuManager_Test.cs
+++ Assets/1. Main Project/Scripts/Menu/MenuManager_Test.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
@@ -24,22 +21,33 @@ public class MenuManager_Test : MonoBehaviour
     }
 
     public int playersSelected = 0;
+    
     public static MenuManager_Test Instance;
     [SerializeField] private Menu_test[] menus;
     [SerializeField] private GameObject player1, player2;
     [SerializeField] private Button startButton;
     private bool player1Selected = false;
     private bool player2Selected = false;
+    [SerializeField] private bool playersReadyToStart = false;
     public JugadoresMinimos jugadoresMinimos;
     [SerializeField] Button player1Button, player2Button;
 
     private void Update()
     {
-        if ( playersSelected >= (int)jugadoresMinimos)
+        if (playersSelected == (int)jugadoresMinimos)
+        {
+            playersReadyToStart = true;
+        }
+        else
+        {
+            playersReadyToStart = false;
+        }
+        
+        if (playersReadyToStart == true)
         {
             startButton.interactable = true;
         }
-        else
+        else if (playersReadyToStart == false)
         {
             startButton.interactable = false;
         }
@@ -52,9 +60,9 @@ public class MenuManager_Test : MonoBehaviour
 
     public void OpenMenu(string menuName)
     {
-        for(int i=0; i < menus.Length; i++)
+        for (int i = 0; i < menus.Length; i++)
         {
-            if(menus[i].menuName == menuName)
+            if (menus[i].menuName == menuName)
             {
                 menus[i].Open();
             }
@@ -67,7 +75,7 @@ public class MenuManager_Test : MonoBehaviour
 
     public void OpenMenu(Menu_test menu)
     {
-        for(int i=0; i < menus.Length; i++)
+        for (int i = 0; i < menus.Length; i++)
         {
             if (menus[i].open)
             {
@@ -81,33 +89,78 @@ public class MenuManager_Test : MonoBehaviour
     {
         menu.Close();
     }
-    
+
     [PunRPC]
     public void Player1Selected()
     {
         player1.GetComponent<Image>().color = Color.green;
-        Destroy(player2);
+        //Destroy(player2);
         player1Selected = true;
-        playersSelected += 1;
+        playersSelected++;
         photonView.RPC("UpdatePlayersSelected", RpcTarget.All, playersSelected);
         player1Button.interactable = false;
+        player2Button.interactable = false;
+        photonView.RPC("DisablePlayer1", RpcTarget.All);
+        player2Button.onClick.RemoveAllListeners();
+    }
+    
+   
+    
+    [PunRPC]
+    public void UpdatePlayersSelected(int newPlayersSelected)
+    {
+        playersSelected = newPlayersSelected;
     }
 
     [PunRPC]
     public void Player2Selected()
     {
-        player2.GetComponent<Image>().color = Color.green;
-        Destroy(player1);
-        player2Selected = true;
-        playersSelected += 1;
-        photonView.RPC("UpdatePlayersSelected", RpcTarget.All, playersSelected);
-        player2Button.interactable = false;
+       
+            player2.GetComponent<Image>().color = Color.green;
+            //Destroy(player1);
+            player2Selected = true;
+            playersSelected++;
+            photonView.RPC("UpdatePlayersSelected", RpcTarget.All, playersSelected);
+            player1Button.interactable = false;
+            player2Button.interactable = false;
+            photonView.RPC("DisablePlayer2", RpcTarget.All);
+            player1Button.onClick.RemoveAllListeners();
+        
     }
 
     [PunRPC]
-    public void UpdatePlayersSelected(int newPlayersSelected)
+    public void ResetSelection()
     {
-        playersSelected = newPlayersSelected;
+        if (player1Selected == true)
+        {
+            player1Selected = false;
+            player1Button.interactable = true;
+            player2Button.interactable = false;
+            player1.GetComponent<Image>().color = Color.white;
+            photonView.RPC("UpdatePlayersSelected", RpcTarget.All, playersSelected);
+            playersSelected --;
+            
+        }
+        else if (player2Selected == true)
+        {
+            player2Selected = false;
+            player2Button.interactable = true;
+            player1Button.interactable = false;
+            player2.GetComponent<Image>().color = Color.white;
+            photonView.RPC("UpdatePlayersSelected", RpcTarget.All, playersSelected);
+            playersSelected --;
+        }
+    }
+    
+    [PunRPC]
+    public void DisablePlayer1()
+    {
+        player1Button.interactable = false;
+    }
+    [PunRPC]
+    public void DisablePlayer2()
+    {
+        player2Button.interactable = false;
     }
     
     public void CloseGame()
