a8d89c9f52f6e419837cf29cdb179887195fb9e4
BigNugget <slondonogil202@gmail.com>
2023-04-25 22:17:01 -0500

    fix3 lmao

--- Assets/1. Main Project/Scripts/Utilities/CanvasCamera.cs
+++ Assets/1. Main Project/Scripts/Utilities/CanvasCamera.cs
@@ -9,6 +9,12 @@ public class CanvasCamera : MonoBehaviour
     public Canvas cavascamera;
     void Start()
     {
+        StartCoroutine(CanvAsassign());
+    }
+
+    IEnumerator CanvAsassign()
+    {
+        yield return new WaitForSeconds(0.1f);
         cavascamera.worldCamera = GameObject.Find("ExpCinemachineBrain").GetComponent<Camera>();
     }
 
