65c47d5cbcd8d83ca8ed30ec2e58148b01aba0e9
QuantumBreak3678 <68030853+juandagongoradiaz@users.noreply.github.com>
2023-05-07 23:54:41 -0500

    Puede ser

--- Assets/ExploradorBikeAnimationsManager.cs
+++ Assets/ExploradorBikeAnimationsManager.cs
@@ -9,13 +9,21 @@ public class ExploradorBikeAnimationsManager : MonoBehaviour
     public Transform LeftMediumTransform;
     public Transform LeftFinalTransform;
     public Transform CameraFinalPosition;
+    public Transform CameraMediumPosition;
     public GameObject playerMesh;
     public GameObject BikeCamera;
     public hoverCraft hoverCraft;
     public hoverCraftTilt hoverCraftTilt; 
     
-    public float lerpSpeed = 1f;
+    public float cameraInterpolationSpeed = 2f;
+    
 
+    
+    public void TransformCameraMediumPosition()
+    {
+        BikeCamera.transform.position = CameraMediumPosition.position; 
+        BikeCamera.transform.rotation = CameraMediumPosition.rotation;
+    }
     public void BikeLeftFinalTransform()
     {
         StartCoroutine(DoBikeLeftFinalTransform());
@@ -24,14 +32,24 @@ public class ExploradorBikeAnimationsManager : MonoBehaviour
     private IEnumerator DoBikeLeftFinalTransform()
     {
         yield return new WaitForSeconds(0.1f); // Espera 0.1 segundos antes de hacer la transformación
-       
-        playerMesh.transform.position = LeftFinalTransform.position; 
+
+        playerMesh.transform.position = LeftFinalTransform.position;
         playerMesh.transform.rotation = LeftFinalTransform.rotation;
-        BikeCamera.transform.position = CameraFinalPosition.position; 
-        BikeCamera.transform.rotation = CameraFinalPosition.rotation;
-   
+
+        float t = 0.0f; // Factor de interpolación inicial
+        while (t < 1.0f) // Mientras no se haya completado la interpolación
+        {
+            t += Time.deltaTime * cameraInterpolationSpeed; // Incrementa el factor de interpolación en función del tiempo y la velocidad de interpolación
+
+            // Interpola la posición y la rotación de la cámara
+            BikeCamera.transform.position = Vector3.Lerp(CameraMediumPosition.position, CameraFinalPosition.position, t);
+            BikeCamera.transform.rotation = Quaternion.Lerp(CameraMediumPosition.rotation, CameraFinalPosition.rotation, t);
+
+            yield return null; // Espera al siguiente frame antes de continuar
+        }
+
         hoverCraft.enabled = true;
-        hoverCraftTilt.enabled = true; 
+        hoverCraftTilt.enabled = true;
     }
     
     public void BikeLeftMediumTransform()
