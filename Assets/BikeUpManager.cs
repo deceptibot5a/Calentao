3de3b4665a8e3a33237c5fec068c75c1223b8bcb
QuantumBreak3678 <68030853+juandagongoradiaz@users.noreply.github.com>
2023-05-08 15:35:36 -0500

    Avances

---
+++ Assets/BikeUpManager.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BikeUpManager : MonoBehaviour
+{
+    public bool caninteract = false;
+    public CanvasGroup fadeManager;
+    public ExploradorPlayerController exploradorPlayerController;
+    public ExploradorAnimatorManager exploradorAnimatorManager;
+    public ExploradorCameraManager ExploradorCameraManager;
+    public CharacterController characterController;
+    public SkinnedMeshRenderer meshRenderer; 
+    
+    public AudioSource _walkAudioSource;
+    public AudioSource _runAudioSource;
+    public  AudioSource _breathingNormalAudioSource;
+    public  AudioSource _breathingRunAudioSource;
+
+   
+    private void OnTriggerEnter(Collider other) 
+    { 
+        if (other.gameObject.CompareTag("BikeUp"))
+        {
+            Debug.Log("hola");
+            caninteract = true; 
+            
+
+        }
+    }
+    
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && (caninteract))
+        {
+            caninteract = false; 
+            fadeManager.DOFade(1f, 1f);
+            exploradorAnimatorManager.enabled = false;
+            exploradorPlayerController.enabled = false;
+            ExploradorCameraManager.enabled = false;
+            characterController.enabled = false;
+            meshRenderer.enabled = false; 
+            
+            
+            StartCoroutine(FadeAudios());
+            
+        } 
+    }
+        private IEnumerator FadeAudios()
+        {
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log("AudiosFades");
+            if (!_breathingNormalAudioSource.isPlaying)
+            {
+                _breathingNormalAudioSource.Play();
+            }
+            float fadeTime = 0.5f; // tiempo para desvanecer el sonido
+
+            // Desvanece el volumen del _breathingNormalAudioSource
+            float startVolume = _breathingNormalAudioSource.volume;
+            float endVolume = 0.2f;
+            float t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _breathingNormalAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _breathingNormalAudioSource.volume = endVolume;
+
+            // Desvanece el volumen del _breathingRunAudioSource
+            startVolume = _breathingRunAudioSource.volume;
+            endVolume = 0f;
+            t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _breathingRunAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _breathingRunAudioSource.volume = endVolume;
+
+            // Desvanece el volumen del _walkAudioSource
+            startVolume = _walkAudioSource.volume;
+            endVolume = 0f;
+            t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _walkAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _walkAudioSource.volume = endVolume;
+
+            // Desvanece el volumen del _runAudioSource
+            startVolume = _runAudioSource.volume;
+            endVolume = 0f;
+            t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _runAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _runAudioSource.volume = endVolume;
+        }
+}
