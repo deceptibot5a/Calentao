a92e3b9361e7fe01a56796e8c3a42694372f5f6f
juandagongoradiaz <68030853+juandagongoradiaz@users.noreply.github.com>
2023-05-12 22:24:31 -0500

    fdgd

---
+++ Assets/1. Main Project/Prefabs/Characters/Explorador/Animations/Bomb/PlantBombManager.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantBombManager : MonoBehaviour
+{
+    public Animator animator;
+    private bool planting;
+    private bool canPlant = false; 
+    public ExploradorPlayerController exploradorPlayerController;
+    public ExploradorAnimatorManager exploradorAnimatorManager;
+    public ExploradorCameraManager ExploradorCameraManager;
+    public CharacterController characterController;
+
+    public GameObject cameraMain;
+    public GameObject bombCamera; 
+    
+    [Header("Player Audios")]
+    public AudioSource _walkAudioSource;
+    public AudioSource _runAudioSource;
+    public  AudioSource _breathingNormalAudioSource;
+    public  AudioSource _breathingRunAudioSource;
+    
+    
+    private void OnTriggerEnter(Collider other) 
+    { 
+        if (other.gameObject.CompareTag("PlantArea"))
+        {
+            Debug.Log("Puede plantar ");
+            canPlant = true; 
+
+        }
+    }
+    
+    private IEnumerator FadePlayerAudios()
+        {
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log("AudiosFades");
+            if (!_breathingNormalAudioSource.isPlaying)
+            {
+                _breathingNormalAudioSource.Play();
+            }
+            float fadeTime = 0.1f; // tiempo para desvanecer el sonido
+
+            // Desvanece el volumen del _breathingNormalAudioSource
+            float startVolume = _breathingNormalAudioSource.volume;
+            float endVolume = 0.2f;
+            float t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _breathingNormalAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _breathingNormalAudioSource.volume = endVolume;
+
+            // Desvanece el volumen del _breathingRunAudioSource
+            startVolume = _breathingRunAudioSource.volume;
+            endVolume = 0f;
+            t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _breathingRunAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _breathingRunAudioSource.volume = endVolume;
+
+            // Desvanece el volumen del _walkAudioSource
+            startVolume = _walkAudioSource.volume;
+            endVolume = 0f;
+            t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _walkAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _walkAudioSource.volume = endVolume;
+
+            // Desvanece el volumen del _runAudioSource
+            startVolume = _runAudioSource.volume;
+            endVolume = 0f;
+            t = 0f;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                _runAudioSource.volume = Mathf.Lerp(startVolume, endVolume, t / fadeTime);
+                yield return null;
+            }
+            _runAudioSource.volume = endVolume;
+            
+            yield return new WaitForSeconds(1f);
+           
+           
+            
+        }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlantArea"))
+        {
+            Debug.Log("No puede plantar");
+            canPlant = false;
+            planting = false; 
+
+        }
+    }
+    void Update()
+    {
+        if (canPlant)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (!planting)
+                {
+                    StartPlanting();
+                }
+            }
+            else if (planting)
+            {
+                CancelPlanting();
+            }
+        }
+       
+    }
+
+    void StartPlanting()
+    {
+        StartCoroutine(FadePlayerAudios());
+        cameraMain.SetActive(false);
+        bombCamera.SetActive(true);
+        planting = true;
+        exploradorAnimatorManager.enabled = false;
+        exploradorPlayerController.enabled = false;
+        ExploradorCameraManager.enabled = false;
+        characterController.enabled = false;
+        animator.SetBool("Planting", true);
+      
+    }
+
+    void CancelPlanting()
+    {
+        
+        cameraMain.SetActive(true);
+        bombCamera.SetActive(false);
+        planting = false;
+        exploradorAnimatorManager.enabled = true;
+        exploradorPlayerController.enabled = true;
+        ExploradorCameraManager.enabled = true;
+        characterController.enabled = true;
+        animator.SetBool("Planting", false);
+        // Aquí puedes agregar el código para detener la lógica de la bomba
+    }
+}
