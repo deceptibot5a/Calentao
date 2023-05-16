08f0ebce03b5d6f60428cfb77eeab37e8e467fac
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-05-16 02:19:06 -0500

    Peces (Falta pulir por problema de origen objeto)

--- Assets/1. Main Project/Scripts/puzzle2/DisparoPez.cs
+++ Assets/1. Main Project/Scripts/puzzle2/DisparoPez.cs
@@ -1,17 +1,13 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
-using Unity.VisualScripting;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class DisparoPez : MonoBehaviour
 {
-     public GameObject fishPrefab;
+    public GameObject fishPrefab;
     public ParticleSystem preshotParticleSystem;
-    public float minShootDelay = 1f;
-    public float maxShootDelay = 3f;
+    public float minShootDelay;
+    public float maxShootDelay;
     public PhotonView photonView;
     private bool canShoot = true;
 
@@ -22,42 +18,53 @@ public class DisparoPez : MonoBehaviour
 
     private void Update()
     {
-        if (PhotonNetwork.IsMasterClient && canShoot) // Solo el master client dispara los peces
+        if (PhotonNetwork.IsMasterClient && canShoot)
         {
-            float delay = Random.Range(minShootDelay, maxShootDelay);
-            photonView.RPC("ShootFishRPC", RpcTarget.All, delay);
-            canShoot = false;
+            photonView.RPC("ShootFishWithDelay", RpcTarget.All);
         }
     }
+
     [PunRPC]
-    private void ShootFishRPC(float delay)
+    private IEnumerator ShootFishWithDelay()
     {
-        StartCoroutine(ShootFish(delay));
+        canShoot = false;
+        float delay = Random.Range(minShootDelay, maxShootDelay);
+        yield return new WaitForSeconds(delay);
+
+        StartCoroutine(ShootFishCoroutine(3f));
     }
+
     
-    private IEnumerator ShootFish(float delay)
+    private IEnumerator ShootFishCoroutine(float delay)
     {
-        yield return new WaitForSeconds(delay-2f);
+        yield return new WaitForSeconds(delay);
 
         preshotParticleSystem.Play();
-        
-        
         yield return new WaitForSeconds(2f);
 
-        GameObject fishObject = Instantiate(fishPrefab, transform.position, Quaternion.identity);
-        fishObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 900f);
+        ShootFish();
+        
 
-       Fish fish = fishObject.GetComponent<Fish>();
-        fish.OnFishHitGround += OnFishHitGround;
-    }
-
-    private void OnFishHitGround(Fish fish)
-    {
-        preshotParticleSystem.Stop();
-        fish.OnFishHitGround -= OnFishHitGround;
-        Destroy(fish.gameObject);
         canShoot = true;
     }
+
+    private void ShootFish()
+    {
+        LeanTween.moveLocalY(fishPrefab, 6.5f, 1.2f).setEaseInOutQuart().setOnComplete(ReturnToWater);
+        
+    }
+
+    private void ReturnToWater()
+    {
+        LeanTween.moveLocalY(fishPrefab, -8f, 1.8f).setEaseOutSine().setOnComplete(RestartRotationX);
+        LeanTween.rotateLocal(fishPrefab, new Vector3(-90f, 0, 0), 0.6f).setEaseInQuad();
+        preshotParticleSystem.Stop();
+    }
+    public void RestartRotationX()
+    {
+        LeanTween.rotateLocal(fishPrefab, new Vector3(90f, 0, 0), 0.2f);
+    }
+    
 }
 
 
