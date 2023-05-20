a0d3038154c58016ae4173c144aba82894841ed6
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-05-20 14:40:28 -0500

    CheckpointFixed

--- Assets/1. Main Project/Scripts/Utilities/Checkpoints.cs
+++ Assets/1. Main Project/Scripts/Utilities/Checkpoints.cs
@@ -2,29 +2,55 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using Unity.VisualScripting;
+
 
 public class Checkpoints : MonoBehaviour
 {
     public Transform checkpoint;
     public CanvasGroup deathPanel;
-    public float fadeTime = 0.3f;
+    public bool isDead;
     
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player1"))
         {
-            TurnOnDeathPanel();
-            other.GetComponent<Transform>().position = checkpoint.position;
+            StartCoroutine(TurnTrue());
+            if (isDead = true)
+            {
+                other.transform.position = checkpoint.position;
+                isDead = false;
+            }
+
+            StartCoroutine(FadeInAndOut());
+            Debug.Log("Toque el checkpoint");
         }
+    }
+    
+    
+    IEnumerator FadeInAndOut()
+    {
+        yield return new WaitForSeconds(0.01f);
+        TurnOnDeathPanel();
+        yield return new WaitForSeconds(1f);
+        TurnOffDeathPanel();
+        Debug.Log("si hice el fade");
     }
 
     public void TurnOnDeathPanel()
     {
-    LeanTween.alphaCanvas(deathPanel, 1f, 0.3f).setOnComplete(TurnOffDeathPanel);
+    LeanTween.alphaCanvas(deathPanel, 1f, 1f).setOnComplete(TurnOffDeathPanel);
     }
     public void TurnOffDeathPanel()
     {
-        LeanTween.alphaCanvas(deathPanel, 0f, 0.6f);
+        LeanTween.alphaCanvas(deathPanel, 0f, 1f);
     }
+    IEnumerator TurnTrue()
+    {
+        yield return new WaitForSeconds(6f);
+        isDead = true;
+        Debug.Log("isDead = true");
+    }
+    
     
 }
