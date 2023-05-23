c406a317b482727a40ddbecc9be4313ed9d990d8
Junebouvier <isamarmery@gmail.com>
2023-05-23 14:44:43 -0500

    Update colliders

--- Assets/1. Main Project/Scripts/Utilities/PauseMenu.cs
+++ Assets/1. Main Project/Scripts/Utilities/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Calentao.PlayerContol;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 using Photon.Pun;
 using UnityEngine.UI;
