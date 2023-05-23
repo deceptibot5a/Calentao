b6dccbc6cc988769fb750217f877e0f24f2a0809
juandagongoradiaz <68030853+juandagongoradiaz@users.noreply.github.com>
2023-05-23 17:23:00 -0500

    Soniditos UI menu

--- Assets/1. Main Project/Scripts/Menu/PressAnyKey.cs
+++ Assets/1. Main Project/Scripts/Menu/PressAnyKey.cs
@@ -1,22 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class PressAnyKey : MonoBehaviour
 {
     public GameObject welcomePanel;
     public GameObject background;
+    public PlayableDirector musicDirector;
+    public UI_SFX SFX; 
     private void FixedUpdate()
     {
         if (Input.anyKey && MenuManager_Test.Instance.isLoading == false)
         {
+            SFX.clickButton();
+            musicDirector.Play();
             background.GetComponent<Image>().raycastTarget = false;
             welcomePanel.GetComponent<PressAnyKey>().enabled = false;
             LeanTween.alpha(welcomePanel.GetComponent<RectTransform>(), 0f, 0.3f).setOnComplete(DeactivateWelcomePanel); 
             LeanTween.alphaCanvas(MenuManager_Test.Instance.loadingPlanetImage.GetComponent<CanvasGroup>(), 0f, 0.3f);
+          
+    
+           
         }
     }
     
