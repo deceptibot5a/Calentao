23f5ce5652341e7e8aceda1a76af3f7fcbfd120b
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-05-23 23:48:45 -0500

    Cambios IMPORTANTES

--- Assets/1. Main Project/Scripts/Player1/PuzzleInteractions.cs
+++ Assets/1. Main Project/Scripts/Player1/PuzzleInteractions.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using Photon.Pun;
+using Unity.VisualScripting;
 
 public class PuzzleInteractions : MonoBehaviour
 {
 
     public CinemachineVirtualCamera puzzlecam;
-
+    
+    public CinemachineBrain cinemachineBrain;
+    
     public int PuzzleNumber;
 
     public InteractionsPlayer1 interactions;
 
+    private void Start()
+    {
+        AssingPlayer();
+    }
+
+    IEnumerator AssingPlayer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        cinemachineBrain = GameObject.FindGameObjectWithTag("Player1").GetComponentInChildren<CinemachineBrain>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,7 +35,7 @@ public class PuzzleInteractions : MonoBehaviour
             interactions.uiInteraction();
             interactions.activecamera = puzzlecam;
             interactions.puzzletype = PuzzleNumber;
-
+            cinemachineBrain.m_DefaultBlend.m_Style = CinemachineBlendDefinition.Style.EaseOut;
         }
     }
     
